0b96f819442830876aa5de2ec2081a9b346a532b
Melanie Stoeckle <mpstoeckle@gmail.com>
2017-10-13 13:57:30 -0400

    connected random number index to data objects.  it works now yayayay

--- app/main.js
+++ app/main.js
@@ -1,8 +1,10 @@
 Vue.component('sortingHatMessage', {
 
-	template: '<p>I know... <strong>{{ name }}</strong>!</p>',
+	template: '<p>I know... <strong>{{ name }}</strong>!\
+						<br><br>\
+						<i>{{description}}</i> </p>',
 
-	props: ['name']
+	props: ['name', 'description']
 
 });
 
@@ -12,29 +14,25 @@ var demo = new Vue({
 
 	data: {
 
-		selectedHouse: '',
+		index: 0,
 		
 		'Houses': [
 
 	    {
 	      name: 'Gryffindor',
-	      description: 'With a lion as its crest and Professor McGonagall at its head, Gryffindor is the house which most values the virtues of courage, bravery and determination.',
-	      index: 1
+	      description: 'With a lion as its crest and Professor McGonagall at its head, Gryffindor is the house which most values the virtues of courage, bravery and determination.'
 	    },
 	    {
 	      name: 'Hufflepuff',
-	      description: 'Hufflepuffs value hard work, patience, loyalty, and fair play. The house has produced its share of great wizards – not least Newt Scamander, author of Fantastic Beasts and Where to Find Them.',
-	      index: 2
+	      description: 'Hufflepuffs value hard work, patience, loyalty, and fair play.  The house has produced its share of great wizards, not least Newt Scamander, author of Fantastic Beasts and Where to Find Them.'
 	    },
 	    {
 	      name: 'Ravenclaw',
-	      description: 'Ravenclaws prize wit, learning, and wisdom. It\'s an ethos etched into founder Rowena Ravenclaw diadem: Wit beyond measure is man\'s greatest treasure.',
-	      index: 3
+	      description: 'Ravenclaws prize wit, learning, and wisdom.  It\'s an ethos etched into founder Rowena Ravenclaw diadem: Wit beyond measure is man\'s greatest treasure.'
 	    },
 	    {
 	      name: 'Slytherin',
-	      description: 'Slytherin produces more than its share of Dark wizards, but also turns out leaders who are proud, ambitious and cunning. Merlin is one particularly famous Slytherin.',
-	      index: 4
+	      description: 'Slytherin produces more than its share of Dark wizards, but also turns out leaders who are proud, ambitious and cunning.  Merlin is one particularly famous Slytherin.'
 	    }
 
 	  ]
@@ -43,13 +41,9 @@ var demo = new Vue({
 
 	methods: {
 
-		sortMe: function(index) {
-			this.randomize();
-		},
-
-		randomize: function() {
-			var index = Math.round(Math.random() * (4 - 1) + 1);
-			console.log("Index: ", index);
+		sortMe: function() {
+			this.index = Math.round(Math.random() * (4 - 1));
+			console.log("Index: ", this.index);
 		}
 
 	}

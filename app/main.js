ea8e86cbd8d4870676992596612047983c259be6
Melanie Stoeckle <mpstoeckle@gmail.com>
2017-10-13 11:15:33 -0400

    created basic component for the sorting hat result, need to make it show only once now instead of whole array

--- app/main.js
+++ app/main.js
@@ -1,43 +1,55 @@
+Vue.component('selectedHouse', {
+
+	template: '<p>I know... <strong>{{ name }}</strong>!</p>',
+
+	props: ['name']
+
+});
+
 var demo = new Vue({
 
-el: '#main',
+	el: '#main',
 
-data: {
+	data: {
 
-	'Houses': [
+		sorted: false,
+		index: 0,
 
-    {
-      name: 'Gryffindor',
-      description: 'With a lion as its crest and Professor McGonagall at its head, Gryffindor is the house which most values the virtues of courage, bravery and determination.',
-      index: 1
-    },
-    {
-      name: 'Hufflepuff',
-      description: 'Hufflepuffs value hard work, patience, loyalty, and fair play. The house has produced its share of great wizards – not least Newt Scamander, author of Fantastic Beasts and Where to Find Them.',
-      index: 2
-    },
-    {
-      name: 'Ravenclaw',
-      description: 'Ravenclaws prize wit, learning, and wisdom. It\'s an ethos etched into founder Rowena Ravenclaw diadem: Wit beyond measure is man\'s greatest treasure.',
-      index: 3
-    },
-    {
-      name: 'Slytherin',
-      description: 'Slytherin produces more than its share of Dark wizards, but also turns out leaders who are proud, ambitious and cunning. Merlin is one particularly famous Slytherin.',
-      index: 4
-    }
+		'Houses': [
 
-  ]
+	    {
+	      name: 'Gryffindor',
+	      description: 'With a lion as its crest and Professor McGonagall at its head, Gryffindor is the house which most values the virtues of courage, bravery and determination.',
+	      index: 1
+	    },
+	    {
+	      name: 'Hufflepuff',
+	      description: 'Hufflepuffs value hard work, patience, loyalty, and fair play. The house has produced its share of great wizards – not least Newt Scamander, author of Fantastic Beasts and Where to Find Them.',
+	      index: 2
+	    },
+	    {
+	      name: 'Ravenclaw',
+	      description: 'Ravenclaws prize wit, learning, and wisdom. It\'s an ethos etched into founder Rowena Ravenclaw diadem: Wit beyond measure is man\'s greatest treasure.',
+	      index: 3
+	    },
+	    {
+	      name: 'Slytherin',
+	      description: 'Slytherin produces more than its share of Dark wizards, but also turns out leaders who are proud, ambitious and cunning. Merlin is one particularly famous Slytherin.',
+	      index: 4
+	    }
 
-},
+	  ]
 
-methods: {
+	},
 
-	sortMe: function() {
-		alert('I am thinking of where to sort you...');
+	methods: {
+
+		sortMe: function() {
+			alert('I am think of where to sort you...')
+			// this.sorted = !this.sorted;
+		}
+
 	}
-
-}
 
 
 });
